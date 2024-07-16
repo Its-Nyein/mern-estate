ebcb2cd6d1dcc76adca7e8a0579ae1fa6a294f58
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-07-17 02:15:48 +0630

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-800 shadow-md">
+      <div className="flex items-center justify-between max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold flex flex-wrap text-sm sm:text-xl">
+            <span className="text-slate-200">Real</span>
+            <span className="text-slate-400">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-[#38bdf81a] flex items-center rounded-xl p-3">
+          <input
+            type="text"
+            placeholder="Search . . ."
+            className="bg-transparent text-[#38bdf8] focus:outline-none w-64 sm:w-25 placeholder-[#38bdf8] indent-2"
+          />
+          <FaSearch className="bg-slate-60 text-[#38bdf8] cursor-pointer" />
+        </form>
+        <ul className="flex gap-4 items-center text-gray-50">
+          <Link to="/">
+            <li className="hidden sm:inline hover:underline cursor-pointer hover:text-[#38bdf8]">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline hover:underline cursor-pointer hover:text-[#38bdf8]">
+              About
+            </li>
+          </Link>
+          <Link to="sign-in">
+            <li className="bg-[#38bdf81a] sm:inline cursor-pointer px-4 py-3 rounded-lg">
+              SignIn
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
