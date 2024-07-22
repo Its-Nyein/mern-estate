d0d2221e7ddfe87d40392f5ef2a9943ffa5ad833
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-07-23 02:33:02 +0630

    chore: update draft UI the Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,49 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 items-center max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-6 text-white">
+        Profile
+      </h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile_avatar"
+          className="rounded-full h-40 w-40 self-center object-cover border-4 border-[#38bdf8]"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <button
+          type="button"
+          className="bg-blue-700/70 p-3 uppercase rounded-lg text-white hover:opacity-90 disabled:opacity-75"
+        >
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between cursor-pointer mt-5">
+        <span className="text-red-600 font-semibold">Delete Account</span>
+        <span className="text-red-600 font-semibold">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
