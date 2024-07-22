8fb64886813c0eaad7c575605c1140cf66a888e9
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-07-23 00:16:24 +0630

    setup google authentication with firebase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,23 @@
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
 
 const OAuth = () => {
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
+      onClick={handleGoogleClick}
       className="flex bg-red-700 p-3 rounded-lg text-white hover:opacity-95 
       disabled:opacity-75 gap-2 text-center justify-center cursor-pointer"
     >
---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-4bffe.firebaseapp.com",
+  projectId: "mern-estate-4bffe",
+  storageBucket: "mern-estate-4bffe.appspot.com",
+  messagingSenderId: "896750566050",
+  appId: "1:896750566050:web:6b3cd5e263ce182b13b5fb",
+  measurementId: "G-LTTFSG4N2D"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
