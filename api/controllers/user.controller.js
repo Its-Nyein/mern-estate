c666db52270f9d5a506d012fb353d6e1aedd8610
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-07-25 01:59:48 +0630

    modify: update update user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+import User from '../models/user.model.js'
+
 export const userController = (req, res) => {
     res.json({
         message: 'API is working well!'
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) next(errorHandler(401, 'Unauthorized'))
+
+    try {
+        if(req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updateUser = await User.findByIdAndUpdate(req.params.id, {
+            // $set : req.body // not good practice
+
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, {new: true})
+        // add new true to get update user res
+
+        const {password, ...rest} = updateUser._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express, { Router } from 'express'
-import { userController } from '../controllers/user.controller.js';
+import { updateUser, userController } from '../controllers/user.controller.js';
+import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.get('/testing', userController)
+router.post('/update/:id',verifyUser, updateUser)
 
 export default router;
