ca52ee92a4d1cbd81a75932e811fbd3e0636cafa
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-01 23:28:24 +0630

    feat: creat update listing API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyUser } from '../utils/verifyUser.js';
-import { deleteListing, listing } from '../controllers/listing.controller.js';
+import { deleteListing, listing, updateListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, listing)
 router.delete('/delete/:id', verifyUser, deleteListing)
+router.post('/update/:id', verifyUser, updateListing)
 
 export default router;
