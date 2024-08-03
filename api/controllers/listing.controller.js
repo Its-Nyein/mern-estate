a90a44de04da70bdb2d89d12dc4abaf230078477
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-04 03:17:29 +0630

    create search API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -55,3 +55,49 @@ export const getListing = async (req, res, next) => {
         next(error)
     }
 }
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        //search in db
+        let offer = req.query.offer;
+        if(offer === undefined || offer === 'false') {
+            offer = {$in: [false, true]}
+        }
+
+        let parking = req.query.parking;
+        if(parking === undefined || parking === 'false') {
+            parking = {$in: [false, true]}
+        }
+
+        let furnished = req.query.furnished;
+        if(furnished === undefined || furnished === 'false') {
+            furnished = {$in: [false, true]}
+        }
+
+        let type = req.query.type;
+        if(type === undefined || type === 'all') {
+            type = {$in: ['rent', 'sale']}
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt'
+        const order = req.query.order || 'desc'
+
+        const listings = await Listing.find({
+            // regex is building fun in mongodb/ options is whatever lower or upper
+            name: {$regex: searchTerm, $options: 'i'},
+            offer,
+            parking,
+            furnished,
+            type
+        }).sort({[sort]: order}).limit(limit).skip(startIndex)
+
+        res.status(200).json(listings)
+    } catch(error) {
+        console.log(error.message)
+        next(error)
+    }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyUser } from '../utils/verifyUser.js';
-import { deleteListing, getListing, listing, updateListing } from '../controllers/listing.controller.js';
+import { deleteListing, getListing, getListings, listing, updateListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create', verifyUser, listing)
 router.delete('/delete/:id', verifyUser, deleteListing)
 router.post('/update/:id', verifyUser, updateListing)
 router.get('/getListing/:id', getListing)
+router.get('/get', getListings)
 
 export default router;
