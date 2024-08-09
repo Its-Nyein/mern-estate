3bd4a22f68c41ee16a4028ae8c8227f12677e708
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-10 05:17:31 +0630

    add the About section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,39 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col gap-3 p-20 mx-auto">
+      <h1 className="text-slate-200 font-bold text-2xl text-center mb-5">
+        <span className="border-b border-gray-500">About M3RNEstate</span>
+      </h1>
+      <div className="text-slate-400 leading-relaxed border border-gray-500 p-5 rounded-md">
+        <p className="my-6">
+          Welcome to M3RNEstate, where your real estate journey begins! As a
+          leading agency specializing in buying, selling, and renting properties
+          in the most sought-after neighborhoods, we are passionate about
+          connecting people with their ideal homes. Our deep market insights and
+          local expertise empower us to navigate the real estate landscape
+          effectively, ensuring that you make informed decisions every step of
+          the way.
+        </p>
+        <p className="my-6">
+          At M3RNEstate, our dedicated team of experienced agents goes beyond
+          traditional real estate practices. We prioritize building meaningful
+          relationships with our clients, understanding that each individual’s
+          needs and goals are unique. Our commitment to exceptional service
+          means we take the time to listen, strategize, and advocate for you,
+          making the buying or selling process not only smooth but also
+          enjoyable
+        </p>
+        <p className="my-6">
+          We believe that every property has a story, and we are here to help
+          you tell yours. Whether you’re seeking your dream home, looking to
+          invest, or aiming to sell for the best price, M3RNEstate is your
+          trusted partner in achieving your real estate aspirations. Let us
+          transform your vision into reality and embark on this exciting journey
+          together!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
