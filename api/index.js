361e475c0b120dc488fa0b1f26a90568c55bfa88
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-12 02:24:22 +0630

    chore: fixed for deploy

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client', 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
 app.listen(3000, '127.0.0.1', () => {
