9a4c22aca97fea7b1edc8fdb5816242df7298433
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-13 22:34:04 +0630

    chore: modify the vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'http://127.0.0.1:3000',
+        secure: false,
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
