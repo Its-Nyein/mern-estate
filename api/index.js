d64df5d796bceaed91011c070ca9243973614861
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-13 21:30:56 +0630

    chore: fix issues

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
-app.listen(3000, '127.0.0.1', () => {
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
